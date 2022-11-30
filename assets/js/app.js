f81cb20b4f61d35820c8a2d56218a579afbb66ff
cannn <ngoccanscitech@gmail.com>
2022-11-30 07:04:30 +0700

    add review reader

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,38 @@ $(function() {
     $('.mainmenu ul li').each(function () {
         $(this).find('ul').css("min-width", $(this).outerWidth(true));
     });
+
+    $(".danhgia-movie-slider").owlCarousel({
+        autoplay: false,
+        loop: true,
+        margin: 20,
+        touchDrag: true,
+        mouseDrag: true,
+        nav: true,
+        navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+        dots: false,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 1200,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            750: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            }
+        }
+    });
 })
